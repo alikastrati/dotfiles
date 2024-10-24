995777d9d9c81a873b62dff5752d6c53dc8a3e35
alikastrati <93733635+alikastrati@users.noreply.github.com>
2024-10-24 18:12:52 +0200

    winconfnvim

---
+++ windows-nvim-config/nvim/lua/config/lazy.lua
@@ -0,0 +1,41 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "plugins" },
+    {"sbdchd/neoformat"},
+    { "ellisonleao/gruvbox.nvim" },
+    { "preservim/nerdcommenter" },
+    { "neovim/nvim-lspconfig" },
+    { "kabouzeid/nvim-lspinstall" },
+  },
+
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+-- Set the colorscheme to gruvbox
+vim.cmd([[colorscheme gruvbox]])
